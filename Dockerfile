a02bc56a8a64cfcc4673e1842e4bcd3f95f33067
Travus <realtravus@outlook.com>
2023-02-16 01:53:36 +0100

    Save pastes to file system

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,8 @@ RUN chown nobody:nogroup /sources/target/release/bin
 FROM debian:bullseye-slim
 COPY --from=builder /sources/target/release/bin /pastebin
 
-USER nobody
+RUN mkdir /srv/pastes
+RUN chown 1000:1000 /srv/pastes
+USER 1000
 EXPOSE 8000
-ENTRYPOINT ["/pastebin", "0.0.0.0:8000"]
+ENTRYPOINT ["/pastebin", "0.0.0.0:8000", "--paste-dir", "/srv/pastes"]
--- src/io.rs
+++ src/io.rs
@@ -1,52 +1,42 @@
+use crate::PasteRepository;
+
 use actix_web::web::Bytes;
-use linked_hash_map::LinkedHashMap;
 use once_cell::sync::Lazy;
-use parking_lot::RwLock;
-use rand::{distributions::Uniform, thread_rng, Rng};
+use std::{
+    fs,
+    io::{Read, Write},
+    path::Path,
+};
 
-pub type PasteStore = RwLock<LinkedHashMap<String, Bytes>>;
+pub struct PasteDiskRepository {}
 
-static BUFFER_SIZE: Lazy<usize> = Lazy::new(|| argh::from_env::<crate::BinArgs>().buffer_size);
+static PASTE_DIR: Lazy<String> = Lazy::new(|| argh::from_env::<crate::BinArgs>().paste_dir);
 
-/// Ensures `ENTRIES` is less than the size of `BIN_BUFFER_SIZE`. If it isn't then
-/// `ENTRIES.len() - BIN_BUFFER_SIZE` elements will be popped off the front of the map.
-///
-/// During the purge, `ENTRIES` is locked and the current thread will block.
-fn purge_old(entries: &mut LinkedHashMap<String, Bytes>) {
-    if entries.len() > *BUFFER_SIZE {
-        let to_remove = entries.len() - *BUFFER_SIZE;
+impl PasteRepository for PasteDiskRepository {
+    fn create(&self, id: &str, content: Bytes) -> Result<(), std::io::Error> {
+        let path = Path::new(&*PASTE_DIR);
+        let mut file = fs::File::create(path.join(id))?;
+        file.write_all(&content)?;
+        file.flush()?;
+        Ok(())
+    }
 
-        for _ in 0..to_remove {
-            entries.pop_front();
+    fn read(&self, id: &str) -> Option<Bytes> {
+        let path = Path::new(&*PASTE_DIR);
+        match fs::File::open(path.join(id)) {
+            Ok(mut file) => {
+                let mut contents = Vec::new();
+                match file.read_to_end(&mut contents) {
+                    Ok(_) => Some(Bytes::from(contents)),
+                    Err(_) => None,
+                }
+            }
+            Err(_) => None,
         }
     }
-}
 
-/// Generates a random id, avoiding confusable characters
-pub fn generate_id() -> String {
-    let valid_chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ"; // Avoids i, l, and o
-    let chars = valid_chars.chars().collect::<Vec<char>>();
-    let range = Uniform::from(0..valid_chars.len());
-    thread_rng()
-        .sample_iter(range)
-        .take(12)
-        .map(|x| chars[x])
-        .collect()
-}
-
-/// Stores a paste under the given id
-pub fn store_paste(entries: &PasteStore, id: String, content: Bytes) {
-    let mut entries = entries.write();
-
-    purge_old(&mut entries);
-
-    entries.insert(id, content);
-}
-
-/// Get a paste by id.
-///
-/// Returns `None` if the paste doesn't exist.
-pub fn get_paste(entries: &PasteStore, id: &str) -> Option<Bytes> {
-    // need to box the guard until owning_ref understands Pin is a stable address
-    entries.read().get(id).map(Bytes::clone)
+    fn exists(&self, id: &str) -> bool {
+        let path = Path::new(&*PASTE_DIR);
+        path.join(id).exists()
+    }
 }
